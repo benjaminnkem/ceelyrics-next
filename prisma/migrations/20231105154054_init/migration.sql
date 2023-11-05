001693eb86e1e3e7f5d4e690bd46f86b935d46ee
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-11-05 16:54:57 +0100

    chore: connected prisma to AWS MySQL database
    - Created CI/CD Pipeline

--- prisma/migrations/20231105154054_init/migration.sql
+++ prisma/migrations/20231105154054_init/migration.sql
@@ -2,7 +2,11 @@
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
