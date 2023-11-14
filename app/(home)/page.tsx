736cffe378ae26733e6047012541a81f62e25ebd
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-11-14 01:55:21 +0100

    fix: hid api key

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,11 @@
 import HomeContent from "@/components/UI/Home";
 import { publicApi } from "@/lib/configs/axiosInstance";
+import { MM_API_KEY } from "@/lib/constants/variables";
 import { InitialTrackResponse } from "@/lib/types/pages";
 
 const getInitialData = async (): Promise<InitialTrackResponse> => {
   const response = await publicApi.get(
-    `/chart.tracks.get?chart_name=top&page=1&page_size=5&country=us&f_has_lyrics=1&apikey=df11018096d8587e8af089c00229c1bd`
+    `/chart.tracks.get?chart_name=top&page=1&page_size=5&country=us&f_has_lyrics=1&apikey=${MM_API_KEY}`
   );
   const { data } = response;
 
