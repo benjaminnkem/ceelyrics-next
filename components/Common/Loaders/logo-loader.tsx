5afa1756e7fe2d9c367428c5e3a7acc3a9d123ea
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-12-03 02:19:18 +0100

    contact us (message) endpoint

--- components/Common/Loaders/logo-loader.tsx
+++ components/Common/Loaders/logo-loader.tsx
@@ -1,6 +1,6 @@
 const LogoLoader: React.FC = () => {
   return (
-    <div className="fixed layer z-[600] top-0 left-0 w-full min-h-screen bg-primary-900 text-white layer-cover">
+    <div className="fixed layer z-[600] top-0 left-0 w-full select-none min-h-screen bg-primary-900 text-white layer-cover">
       <div className="w-full min-h-screen grid text-center place-content-center animate-pulse [animation-duration:3s]">
         <div className="overflow-hidden py-1 border-white flex">
           <p className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-extrabold" id="ceeText">
