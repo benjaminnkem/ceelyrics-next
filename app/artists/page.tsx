05b8443ed2757e3843424a5a5a01aceda24c4e6b
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-02-07 22:23:36 +0100

    update: artists header section

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,4 +1,5 @@
 import ArtistGrid from "@/components/UI/Artists/artist-names";
+import ArtistsHeader from "@/components/UI/Artists/header";
 import HeroSearch from "@/components/UI/Home/hero-search";
 import { BASE_API_URL } from "@/lib/constants";
 import { openSans } from "@/lib/fonts";
@@ -23,44 +24,7 @@ const Artists = async () => {
 
   return (
     <>
-      <header>
-        <div id="jumbo" className="min-h-[40rem] flex items-center relative">
-          <div className="absolute top-0 left-0 w-full overflow-hidden h-full select-none">
-            <Image
-              src={`/images/backgrounds/subscribe_bg.png`}
-              alt="home cover"
-              id="headCoverImage"
-              width={1920}
-              height={800}
-              className="w-full h-full object-cover"
-            />
-          </div>
-
-          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-background-950 to-transparent"></div>
-
-          <div className="absolute top-0 left-0 text-white flex items-center w-full h-full">
-            <div className="container mx-auto">
-              <div className="grid md:grid-cols-5 gap-2 items-center h-full">
-                <div className="space-y-4 text-center md:text-start md:col-span-3 col-span-full">
-                  <div className="overflow-hidden">
-                    <h1
-                      className={`${openSans.className} md:text-5xl text-4xl font-extrabold leading-relaxed jumbo-text`}
-                    >
-                      Find Artists
-                    </h1>
-                  </div>
-                  <div className="overflow-hidden">
-                    <p className="leading-relaxed jumbo-text">Find all available artists.</p>
-                  </div>
-
-                  <HeroSearch />
-                </div>
-                <div className="grid place-content-center"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <ArtistsHeader />
       <main>
         <ArtistGrid artistsResult={artists} />
       </main>
--- components/UI/Home/hero-search.tsx
+++ components/UI/Home/hero-search.tsx
@@ -1,8 +1,6 @@
 import { Search } from "lucide-react";
 
 const HeroSearch = () => {
-  const searchMusic = () => {};
-
   return (
     <div className="overflow-hidden">
       <div className="w-full jumbo-search rounded-md border outline-none px-2 border-white/40 bg-black/20 flex items-center gap-4">
